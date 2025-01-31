315c5edc814aa4d86756b5731f6f194cf9c22390
Joseph Shenton (@moderniselife) <joe@modernise.life>
2025-01-31 14:42:40 +1100

    Fix install script bailing out and set default password to nestos

--- install.sh
+++ install.sh
@@ -3,11 +3,6 @@
 # Create data directories
 mkdir -p /var/lib/nestos/plugins/cloudflared-web/config
 
-# Generate random password if not set
-if [ -z "$BASIC_AUTH_PASS" ]; then
-    BASIC_AUTH_PASS=$(openssl rand -base64 12)
-fi
-
 # Create default configuration
 cat > /var/lib/nestos/plugins/cloudflared-web/.env << EOL
 WEBUI_PORT=14333
@@ -16,7 +11,7 @@ PROTOCOL=auto
 METRICS_ENABLE=false
 METRICS_PORT=60123
 BASIC_AUTH_USER=admin
-BASIC_AUTH_PASS=${BASIC_AUTH_PASS}
+BASIC_AUTH_PASS=nestos
 DATA_DIR=/var/lib/nestos/plugins/cloudflared-web
 EOL
 
